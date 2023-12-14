be65b308d438f93539ea7156d1666447a1f8630a
Nimrod <nimrod@getbaz.com>
2023-12-14 09:46:54 +0200

    Updated python code

--- python/edits.py
+++ python/edits.py
@@ -5,19 +5,20 @@
 # import complex math module
 import cmath
 
-
 a = 1
-b = 2
-c = 6
-c -= 2
+a += 3
+b2 = 5
+b2 *= 2
+c = 4
+
 c += 3
 
 # calculate the discriminant
-d = (b**2) - (4*a*c)
+d = (b2 ** 2) - (4 * a * c)
 
 # find two solutions
-sol1 = (-b-cmath.sqrt(d))/(2*a)
-sol2 = (-b+cmath.sqrt(d))/(2*a)
+sol1 = (-b2 - cmath.sqrt(d)) / (2 * a)
+sol2 = (-b2 + cmath.sqrt(d)) / (2 * a)
 
 print('The solution are {0} and {1}'.format(sol1, sol2))
 
@@ -29,22 +30,21 @@ conv_fac = 0.621371
 
 # calculate miles
 miles = kilometers * conv_fac
-print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
-
+print('%0.2f kilometers is equal to %0.2f miles' % (kilometers, miles))
 
 # Program to add two matrices using nested loop
 
-X = [[12,7,3],
-     [4 ,5,6],
-     [7 ,8,9]]
+X = [[12, 7, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
 
-Y = [[5,8,1],
-     [6,7,3],
-     [4,5,9]]
+Y = [[5, 8, 1],
+     [6, 7, 3],
+     [4, 5, 9]]
 
-result = [[0,0,0],
-          [0,0,0],
-          [0,0,0]]
+result = [[0, 0, 0],
+          [0, 0, 0],
+          [0, 0, 0]]
 
 # iterate through rows
 for i in range(len(X)):
@@ -56,14 +56,14 @@ for r in result:
     print(r)
 
 
-def file_len(fname):
-    with open(fname) as f:
+def file_len(file_name, status):
+    with open(file_name, status) as f:
         for i, l in enumerate(f):
             pass
     return i + 1
 
 
-print(file_len("my_file.txt"))
+print(file_len("my_file.txt", "r"))
 
 count = 0
 
