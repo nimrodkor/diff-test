59dfe7aec50cde35b1b96379942deb6c8ac9a30e
Nimrod <nimrod@getbaz.com>
2023-12-14 09:44:21 +0200

    Initial python code

---
+++ python/additions.py
@@ -0,0 +1,72 @@
+# Solve the quadratic equation ax**2 + bx + c = 0
+
+# import complex math module
+import cmath
+
+a = 1
+b = 5
+c = 6
+
+# calculate the discriminant
+d = (b**2) - (4*a*c)
+
+# find two solutions
+sol1 = (-b-cmath.sqrt(d))/(2*a)
+sol2 = (-b+cmath.sqrt(d))/(2*a)
+
+print('The solution are {0} and {1}'.format(sol1, sol2))
+
+# Taking kilometers input from the user
+kilometers = float(input("Enter value in kilometers: "))
+
+# conversion factor
+conv_fac = 0.621371
+
+# calculate miles
+miles = kilometers * conv_fac
+print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
+
+
+# Program to add two matrices using nested loop
+
+X = [[12,7,3],
+     [4 ,5,6],
+     [7 ,8,9]]
+
+Y = [[5,8,1],
+     [6,7,3],
+     [4,5,9]]
+
+result = [[0,0,0],
+          [0,0,0],
+          [0,0,0]]
+
+# iterate through rows
+for i in range(len(X)):
+    # iterate through columns
+    for j in range(len(X[0])):
+        result[i][j] = X[i][j] + Y[i][j]
+
+for r in result:
+    print(r)
+
+
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+
+print(file_len("my_file.txt"))
+
+count = 0
+
+my_string = "Programiz"
+my_char = "r"
+
+for i in my_string:
+    if i == my_char:
+        count += 1
+
+print(count)
---
+++ python/dir/deletions.py
@@ -0,0 +1,99 @@
+# Solve the quadratic equation ax**2 + bx + c = 0
+
+# import complex math module
+import cmath
+
+a = 1
+b = 5
+c = 6
+
+# calculate the discriminant
+d = (b**2) - (4*a*c)
+
+# find two solutions
+sol1 = (-b-cmath.sqrt(d))/(2*a)
+sol2 = (-b+cmath.sqrt(d))/(2*a)
+
+print('The solution are {0} and {1}'.format(sol1, sol2))
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows):
+    for j in range(i+1):
+        print("* ", end="")
+    print("\n")
+
+# Taking kilometers input from the user
+kilometers = float(input("Enter value in kilometers: "))
+
+# conversion factor
+conv_fac = 0.621371
+
+# calculate miles
+miles = kilometers * conv_fac
+print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
+
+
+# Program to add two matrices using nested loop
+
+X = [[12,7,3],
+     [4 ,5,6],
+     [7 ,8,9]]
+
+Y = [[5,8,1],
+     [6,7,3],
+     [4,5,9]]
+
+result = [[0,0,0],
+          [0,0,0],
+          [0,0,0]]
+
+# iterate through rows
+for i in range(len(X)):
+    # iterate through columns
+    for j in range(len(X[0])):
+        result[i][j] = X[i][j] + Y[i][j]
+
+for r in result:
+    print(r)
+
+
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+
+print(file_len("my_file.txt"))
+
+count = 0
+
+my_string = "Programiz"
+my_char = "r"
+
+for i in my_string:
+    if i == my_char:
+        count += 1
+
+print(count)
+
+
+# Program to sort alphabetically the words form a string provided by the user
+
+my_str = "Hello this Is an Example With cased letters"
+
+# To take input from the user
+#my_str = input("Enter a string: ")
+
+# breakdown the string into a list of words
+words = [word.lower() for word in my_str.split()]
+
+# sort the list
+words.sort()
+
+# display the sorted words
+
+print("The sorted words are:")
+for word in words:
+    print(word)
